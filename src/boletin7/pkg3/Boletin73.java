6878076885f8f1bcac106821fcf325c33fd25800
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2014-11-06 10:25:52 +0100

    Scanner

---
+++ src/boletin7/pkg3/Boletin73.java
@@ -0,0 +1,18 @@
+
+package boletin7.pkg3;
+
+import java.util.Scanner;
+
+public class Boletin73 {
+
+    public static void main(String[] args) {
+      
+        Scanner dato=new Scanner(System.in);
+        Metodo obx= new Metodo();
+        obx.setNumero(dato.nextShort());
+        obx.Resultado();
+    
+    
+    }
+    
+}
