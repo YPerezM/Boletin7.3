ebd2572af980d669164ee58f680ddb0306fc78c5
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2014-11-06 10:29:44 +0100

    Scanner

---
+++ src/boletin7/pkg3/Metodo.java
@@ -0,0 +1,34 @@
+
+package boletin7.pkg3;
+
+public class Metodo {
+     short num1;
+    
+    public Metodo(){
+        
+    }
+    
+    public void setNumero(short v1){
+        num1=v1;
+    }
+    
+    public short getNumero(){
+        return num1;
+    }
+    
+    public void Resultado(){
+        
+        if (num1>0){
+            System.out.println("+");
+        }
+        
+        else if(num1==0){
+            System.out.println("0");    
+        }
+        else{
+        System.out.println("-");
+    }
+    }
+
+    
+}
